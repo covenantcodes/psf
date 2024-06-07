918fbd6d13f3d9ebd9b4325728b632263fffad79
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2024-06-07 14:35:19 +0100

    Added Poppins Font

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 import AppNavigator from "./Navigator/AppNavigator";
 
 const App = () => {
+
+   // FONTS
+   const [fontsLoaded] = useFonts({
+    // POPPINS
+    PoppinsThin: require("./assets/fonts/Poppins/Poppins-Thin.ttf"),
+    PoppinsThinItaltic: require("./assets/fonts/Poppins/Poppins-ThinItalic.ttf"),
+    PoppinsLight: require("./assets/fonts/Poppins/Poppins-Light.ttf"),
+    PoppinsExtraLight: require("./assets/fonts/Poppins/Poppins-ExtraLight.ttf"),
+    PoppinsMedium: require("./assets/fonts/Poppins/Poppins-Medium.ttf"),
+    PoppinsRegular: require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+    PoppinsSemiBold: require("./assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+    PoppinsBold: require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
+    PoppinsExtraBold: require("./assets/fonts/Poppins/Poppins-ExtraBold.ttf"),
+    PoppinsExtraBoldItalic: require("./assets/fonts/Poppins/Poppins-ExtraBoldItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return <AppNavigator />;
 };
 
